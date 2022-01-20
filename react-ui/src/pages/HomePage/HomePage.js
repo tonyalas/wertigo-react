58e1154f5af41e7d151870a601aebb6776a277ed
Tony Alas <tony_alas99@hotmail.com>
2022-01-20 18:48:14 -0500

    updated so that the HomePage.js file has the useEffect hook that makes the title tag Wertigo

--- react-ui/src/pages/HomePage/HomePage.js
+++ react-ui/src/pages/HomePage/HomePage.js
@@ -2,8 +2,13 @@ import Container from 'react-bootstrap/Container';
 import IndexCarousel from '../../components/IndexCarousel/IndexCarousel';
 import IndexMainBody from '../../components/IndexMainBody/IndexMainBody';
 import JumbotronHeader from '../../components/JumbotronHeader/JumbotronHeader';
+import { useEffect } from 'react';
 
 function HomePage() {
+    useEffect(() => {
+        document.title = 'Wertigo'
+    }, []);
+
     return (
         <>
 
